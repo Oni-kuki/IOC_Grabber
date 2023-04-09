e48e83cb108694438d1739b9495b8cebcfb5f6ce
Oni-kuki <oni@disroot.org>
2023-04-09 15:21:57 +0200

    more efficient Hash-parser script

--- Hash-Parser.sh
+++ Hash-Parser.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
+echo "  ##   ##                    ###               ###### ";
+echo "  ##   ##                     ##                ##  ## ";
+echo "  ##   ##   ####     #####    ##                ##  ##   ####    ######    #####    ####    ###### ";
+echo "  #######      ##   ##        #####   ######    #####       ##    ##  ##  ##       ##  ##    ##  ## ";
+echo "  ##   ##   #####    #####    ##  ##            ##       #####    ##       #####   ######    ## ";
+echo "  ##   ##  ##  ##        ##   ##  ##            ##      ##  ##    ##           ##  ##        ## ";
+echo "  ##   ##   #####   ######   ###  ##           ####      #####   ####     ######    #####   #### ";
+echo "  more Easy with that script                    Oni-kuki                                         ";
+
+
 if [ -z "$1" ]; then
-  echo "Usage: Hash-Parser.sh.sh <filename>"
+  echo "Usage: Hash-Parser.sh <filename>"
   exit 1
 fi
-# Parse IOC.txt for Hashes
-  awk ' {print $2 }' "$1" > Hashes.txt && sed '1,3d' Hashes.txt -i
- 
+# Check if file exists
+if [ -e "$1" ]; then
+  # Check if file is empty
+  if [ -s "$1" ]; then
+    # Check if file contains hashes
+    if grep -E -o "([a-fA-F0-9]{32}|[a-fA-F0-9]{40}|[a-fA-F0-9]{64})" "$1" > /dev/null; then
+      echo "Hashes Found"
+      grep -E -o "([a-fA-F0-9]{32}|[a-fA-F0-9]{40}|[a-fA-F0-9]{64})" "$1" > Hashes.txt
+      echo "Hashes Written To File Hashes.txt"
+    else
+      echo "No Hashes in file (MD5, SHA1, SHA256)"
+    fi
+  else
+    echo "File Of Hashes Is Empty"
+  fi
+else
+  echo "File Of Hashes Not Found"
+fi
