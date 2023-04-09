b5ee36ce50d368013988dffd5ec6e018f7dd62b0
Oni-kuki <oni@disroot.org>
2023-04-09 15:57:16 +0200

    Update Hash-Parser.sh

--- Hash-Parser.sh
+++ Hash-Parser.sh
@@ -9,7 +9,7 @@ echo "  ##   ##  ##  ##        ##   ##  ##            ##      ##  ##    ##      
 echo "  ##   ##   #####   ######   ###  ##           ####      #####   ####     ######    #####   #### ";
 echo "  more Easy with that script                    Oni-kuki                                         ";
 
-
+now=`date +%Y-%m-%d_%H-%M-%S`
 if [ -z "$1" ]; then
   echo "Usage: Hash-Parser.sh <filename>"
   exit 1
@@ -21,8 +21,8 @@ if [ -e "$1" ]; then
     # Check if file contains hashes
     if grep -E -o "([a-fA-F0-9]{32}|[a-fA-F0-9]{40}|[a-fA-F0-9]{64})" "$1" > /dev/null; then
       echo "Hashes Found"
-      grep -E -o "([a-fA-F0-9]{32}|[a-fA-F0-9]{40}|[a-fA-F0-9]{64})" "$1" > Hashes.txt
-      echo "Hashes Written To File Hashes.txt"
+      grep -E -o "([a-fA-F0-9]{32}|[a-fA-F0-9]{40}|[a-fA-F0-9]{64})" "$1" > Hashes_"$now".txt
+      echo "Hashes Written To File Hashes_$now.txt"
     else
       echo "No Hashes in file (MD5, SHA1, SHA256)"
     fi
