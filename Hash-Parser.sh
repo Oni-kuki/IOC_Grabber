71ad0216424ac90d5b805d95ee4895348ee7ebbe
Oni-kuki <oni@disroot.org>
2023-04-09 05:51:39 +0200

    Update Hash-Parser.sh

--- Hash-Parser.sh
+++ Hash-Parser.sh
@@ -1,1 +1,8 @@
-awk ' {print $2 }' IOC.txt > Hashes.txt | sed '1,3d' Hashes.txt -i
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: Hash-Parser.sh.sh <filename>"
+  exit 1
+fi
+# Parse IOC.txt for Hashes
+awk ' {print $2 }' "$1" > Hashes.txt | sed '1,3d' Hashes.txt -i
